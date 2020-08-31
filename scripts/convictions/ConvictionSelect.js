1102850494bacee7d9ee74f2f21b3bce8897d83a
j0verton <overtonje@gmail.com>
2020-08-31 14:46:41 -0400

    cors error working on chpt 3

---
+++ scripts/convictions/ConvictionSelect.js
@@ -0,0 +1,48 @@
+/*
+ *   ConvictionSelect component that renders a select HTML element
+ *   which lists all convictions in the Glassdale PD API
+ */
+import { useConvictions, getConvictions } from "./ConvictionProvider.js"
+
+// Get a reference to the DOM element where the <select> will be rendered
+const contentTarget = document.querySelector(".filters__crime")
+
+export const ConvictionSelect = () => {
+    // Get all convictions from application state
+    getConvictions()
+    .then(()=> {
+        const convictions = useConvictions()
+    
+
+        const render = convictionsCollection => {
+            /*
+                Use interpolation here to invoke the map() method on
+                the convictionsCollection to generate the option elements.
+                Look back at the example provided above.
+            */
+
+            // const convictionList = convictons.map(
+            //     convictionObj => {
+            //         const conviction = convictionObj.name;
+            //         return conviction;
+            //     }
+            // )
+
+            contentTarget.innerHTML = `
+                <select class="dropdown" id="crimeSelect">
+                    <option value="0">Please select a crime...</option>
+                    ${
+                        convictons.map(
+                            convictionObj => {
+                                const conviction = convictionObj.name;
+                                return conviction;
+                            }
+                        )
+                    }
+                </select>
+            `
+        }
+
+        render(convictions)
+    })
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 import {OfficerList} from "./officers/OfficerList.js"
 import { CriminalList } from "./criminals/CriminalList.js";
+import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
+import { getConvictions } from "./convictions/ConvictionProvider.js";
+
 
 OfficerList();
 CriminalList();
+ConvictionSelect();
